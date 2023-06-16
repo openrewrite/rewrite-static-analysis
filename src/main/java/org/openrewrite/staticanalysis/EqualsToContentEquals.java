b4877a34f9b97fbee40a5216b1e7d9c0e74488f7
AlekSimpson <alek@asu.me>
2023-06-16 14:08:24 -0700

    polished somethings up and added license headers

--- src/main/java/org/openrewrite/staticanalysis/EqualsToContentEquals.java
+++ src/main/java/org/openrewrite/staticanalysis/EqualsToContentEquals.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.staticanalysis;
 
 import org.openrewrite.ExecutionContext;
@@ -13,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EqualsToContentEquals extends Recipe {
     private static final MethodMatcher equals_matcher = new MethodMatcher("java.lang.String equals(..)");
@@ -55,15 +71,13 @@ public class EqualsToContentEquals extends Recipe {
                     Expression newArg = inv.getSelect();
                     if (inv.getSelect() == null) { return m; }
 
-                    JavaType sb_type = JavaType.buildType("java.lang.StringBuilder");
-                    JavaType string_buffer_type = JavaType.buildType("java.lang.StringBuffer");
-                    JavaType char_sq_type = JavaType.buildType("java.lang.CharSequence");
+                    Stream<JavaType> TYPES = Stream.of(
+                            JavaType.buildType("java.lang.StringBuilder"),
+                            JavaType.buildType("java.lang.StringBuffer"),
+                            JavaType.buildType("java.lang.CharSequence")
+                    );
 
-                    if (
-                            TypeUtils.isOfType(newArg.getType(), sb_type)            ||
-                            TypeUtils.isOfType(newArg.getType(), string_buffer_type) ||
-                            TypeUtils.isOfType(newArg.getType(), char_sq_type)
-                    ) {
+                    if (TYPES.anyMatch(type -> TypeUtils.isOfType(newArg.getType(), type))) {
                         // strip out the toString() on the argument
                         List<Expression> args = new ArrayList<>(1);
                         args.add(newArg);
