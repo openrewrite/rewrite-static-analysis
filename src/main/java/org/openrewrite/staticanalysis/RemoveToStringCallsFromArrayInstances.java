dbc8624963594ff20c67c1e5b90a016b1cf978f9
AlekSimpson <alek@asu.me>
2023-06-19 15:11:32 -0700

    the recipe for the toString() replacement is mostly finished, except for two tests which are testing an edge cases related to String.format()

--- src/main/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstances.java
+++ src/main/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstances.java
@@ -1,16 +1,24 @@
 package org.openrewrite.staticanalysis;
 
 import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RemoveToStringCallsFromArrayInstances extends Recipe {
-    private static final MethodMatcher TOSTRING_MATCHER = new MethodMatcher("java.util.Arrays toString(..)");
+    private static final MethodMatcher TO_STRING_MATCHER = new MethodMatcher("java.lang.Object toString(..)");
+    private static final MethodMatcher PRINTLN_MATCHER = new MethodMatcher("java.io.PrintStream println(..)");
+    private static final MethodMatcher STR_FORMAT_MATCHER = new MethodMatcher("java.lang.String format(..)");
 
     @Override
     public String getDisplayName() {
@@ -23,6 +31,7 @@ public class RemoveToStringCallsFromArrayInstances extends Recipe {
     }
 
     public TreeVisitor<?, ExecutionContext> getVisitor() {
+        //return Preconditions.check(new UsesType<>("java.lang.Object[]", false), new RemoveToStringFromArraysVisitor());
         return new RemoveToStringFromArraysVisitor();
     }
 
@@ -31,18 +40,48 @@ public class RemoveToStringCallsFromArrayInstances extends Recipe {
         public J.MethodInvocation visitMethodInvocation(J.MethodInvocation mi, ExecutionContext ctx) {
             J.MethodInvocation m = super.visitMethodInvocation(mi, ctx);
 
-            if (!TOSTRING_MATCHER.matches(m)) {
+            if (TO_STRING_MATCHER.matches(m)) {
+                String builder_string = "Arrays.toString(#{anyArray(java.lang.String)})";
+                Expression select = m.getSelect();
+                assert select != null;
+
+                return buildReplacement(builder_string, m, select);
+
+            }else if (PRINTLN_MATCHER.matches(m)) {
+                Expression select = m.getArguments().get(0);
+                String builder_string = "System.out.println(Arrays.toString(#{anyArray(java.lang.String)}))";
+
+                return buildReplacement(builder_string, m, select);
+            }else if (STR_FORMAT_MATCHER.matches(m)) {
+                List<Expression> arguments = m.getArguments();
+
+                for (Expression arg : arguments) {
+                    if (arg.getType() instanceof JavaType.Array) {
+                        arg = JavaTemplate.builder("Arrays.toString(#{anyArray(java.lang.String)})")
+                                .imports("java.util.Arrays")
+                                .build()
+                                .apply(getCursor(), arg.getCoordinates().replace(), arg);
+                    }
+                }
+                maybeAddImport("java.util.Arrays");
+
+                return m.withArguments(arguments);
+            }
+
+            return m;
+       }
+
+        public J.MethodInvocation buildReplacement(String builder_string, J.MethodInvocation m, Expression select) {
+            if (!(select.getType() instanceof JavaType.Array)) {
                 return m;
             }
 
-            Expression select = m.getSelect();
-
-            m = JavaTemplate.builder("Arrays.toString(#{java.util.Arrays}")
+            J.MethodInvocation retVal = JavaTemplate.builder(builder_string)
                     .imports("java.util.Arrays")
                     .build()
-                    .apply(getCursor(), m.getCoordinates().replace());
-
-            return m;
+                    .apply(getCursor(), m.getCoordinates().replace(), select);
+            maybeAddImport("java.util.Arrays");
+            return retVal;
         }
     }
 }
--- src/test/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstancesTest.java
+++ src/test/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstancesTest.java
@@ -1,13 +1,15 @@
 package org.openrewrite.staticanalysis;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
+@SuppressWarnings({"ImplicitArrayToString", "UnnecessaryLocalVariable", "RedundantStringFormatCall", "MalformedFormatString", "PrimitiveArrayArgumentToVarargsMethod"})
 public class RemoveToStringCallsFromArrayInstancesTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
@@ -18,6 +20,7 @@ public class RemoveToStringCallsFromArrayInstancesTest implements RewriteTest {
 
     @Test
     @DocumentExample
+    @Issue("https://github.com/openrewrite/rewrite-static-analysis/issues/44")
     public void fixNonCompliantToString() {
         //language=java
         rewriteRun(
@@ -35,6 +38,178 @@ public class RemoveToStringCallsFromArrayInstancesTest implements RewriteTest {
               class SomeClass {
                   public static void main(String[] args) {
                       String argStr = Arrays.toString(args);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void doesNotRunOnNonArrayInstances() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int number = 5;
+                      System.out.println(number.toString());
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void runsOnNonStringArrays() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                      String arrStr = getNumArr().toString();
+                  }
+                  
+                  public int[] getNumArr() {
+                      int[] nums = {1, 2, 3, 4};
+                      return nums;
+                  }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                  public static void main(String[] args) {
+                      String arrStr = Arrays.toString(getNumArr());
+                  }
+                  
+                  public int[] getNumArr() {
+                      int[] nums = {1, 2, 3, 4};
+                      return nums;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void selectIsAMethod() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                      String arrStr = getArr().toString();
+                  }
+                  
+                  public String[] getArr() {
+                      String[] arr = {"test", "array"};
+                      return arr;
+                  }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                  public static void main(String[] args) {
+                      String arrStr = Arrays.toString(getArr());
+                  }
+                  
+                  public String[] getArr() {
+                      String[] arr = {"test", "array"};
+                      return arr;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void printlnEdgeCase() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int[] s = new int[]{1,2,3};
+                      System.out.println(s);
+                  }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int[] s = new int[]{1,2,3};
+                      System.out.println(Arrays.toString(s));
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void stringFormatEdgeCase() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int[] s = new int[]{1, 2, 3};
+                      System.out.println(String.format("s=%s", s));
+                  }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int[] s = new int[]{1, 2, 3};
+                      System.out.println(String.format("s=%s", Arrays.toString(s)));
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    public void stringFormatMultipleArraysPassedIn() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                  public static void main(String[] args) {
+                    int[] s1 = new int[]{1, 2, 3};
+                    int[] s2 = new int[]{4, 5, 6};
+                    
+                    System.out.println(String.format("s1=%s, s2=%s", s1, s2));
+                  }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                  public static void main(String[] args) {
+                      int[] s1 = new int[]{1, 2, 3};
+                      int[] s2 = new int[]{4, 5, 6};
+                      
+                      System.out.println(String.format("s1=%s, s2=%s", Arrays.toString(s1), Arrays.toString(s2)));
                   }
               }
               """
