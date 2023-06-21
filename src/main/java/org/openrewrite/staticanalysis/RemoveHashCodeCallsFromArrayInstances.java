89319a060484dec6addba3be5f3c7a2ebcf64766
AlekSimpson <alek@asu.me>
2023-06-20 20:50:02 -0700

    added first tests for recipe. All tests pass right now

---
+++ src/main/java/org/openrewrite/staticanalysis/RemoveHashCodeCallsFromArrayInstances.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+
+public class RemoveHashCodeCallsFromArrayInstances extends Recipe {
+    private static final MethodMatcher HASHCODE_MATCHER = new MethodMatcher("java.lang.Object hashCode()");
+    @Override
+    public String getDisplayName() {
+        return "hashCode should not be called on array instances";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Removes hashCode method calls and replaces it with Arrays.hashCode(...) call.";
+    }
+
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(Preconditions.or(
+                new UsesMethod<>(HASHCODE_MATCHER)
+        ), new RemoveHashCodeCallsFromArrayInstancesVisitor());
+    }
+
+    private static class RemoveHashCodeCallsFromArrayInstancesVisitor extends JavaIsoVisitor<ExecutionContext> {
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation mi, ExecutionContext ctx) {
+            J.MethodInvocation m = super.visitMethodInvocation(mi, ctx);
+
+            if (HASHCODE_MATCHER.matches(m)) {
+                String builder_string = "Arrays.hashCode(#{anyArray(java.lang.Object)})";
+                Expression select = m.getSelect();
+                assert select != null;
+
+                if (!(select.getType() instanceof JavaType.Array)) {
+                    return m;
+                }
+
+                J.MethodInvocation invocation = JavaTemplate.builder(builder_string)
+                        .imports("java.util.Arrays")
+                        .build()
+                        .apply(getCursor(), m.getCoordinates().replace(), select);
+                maybeAddImport("java.util.Arrays");
+
+                return invocation;
+            }
+
+            return m;
+        }
+    }
+}
---
+++ src/test/java/org/openrewrite/staticanalysis/RemoveHashCodeCallsFromArrayInstancesTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+@SuppressWarnings("ArrayHashCode")
+public class RemoveHashCodeCallsFromArrayInstancesTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion())
+          .recipe(new RemoveHashCodeCallsFromArrayInstances());
+    }
+
+    @Test
+    @DocumentExample
+    @Issue("https://github.com/openrewrite/rewrite-static-analysis/issues/44")
+    void replaceHashCodeCalls() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                public static void main(String[] args) {
+                  int argHash = args.hashCode();
+                }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                public static void main(String[] args) {
+                  int argHash = Arrays.hashCode(args);
+                }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void selectIsAMethod() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                public static void main(String[] args) {
+                  int hashCode = getArr().hashCode();
+                }
+                
+                public int[] getArr() {
+                  return new int[]{1, 2, 3};
+                }
+              }
+              """,
+            """
+              import java.util.Arrays;
+              
+              class SomeClass {
+                public static void main(String[] args) {
+                  int hashCode = Arrays.hashCode(getArr());
+                }
+                
+                public int[] getArr() {
+                  return new int[]{1, 2, 3};
+                }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void onlyRunOnArrayInstances() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              class SomeClass {
+                public static void main(String[] args) {
+                  int name = "bill";
+                  int hashCode = name.hashCode();
+                }
+              }
+              """
+          )
+        );
+    }
+}
