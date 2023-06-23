40167ddb30c7065e789bcfff1e57094e3687d6e0
Knut Wannheden <knut@moderne.io>
2023-06-23 12:40:16 +0200

    The cursor message must be added before the `super` call

--- src/main/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstances.java
+++ src/main/java/org/openrewrite/staticanalysis/RemoveToStringCallsFromArrayInstances.java
@@ -16,13 +16,18 @@
 package org.openrewrite.staticanalysis;
 
 import org.openrewrite.*;
-import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesMethod;
-import org.openrewrite.java.tree.*;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RemoveToStringCallsFromArrayInstances extends Recipe {
@@ -36,7 +41,7 @@ public class RemoveToStringCallsFromArrayInstances extends Recipe {
             "java.lang.StringBuilder append(Object)",
             "java.io.PrintStream format(String, Object[])"
     );
-    private  static final TreeVisitor<?, ExecutionContext> PRECONDITION = Preconditions.or(
+    private static final TreeVisitor<?, ExecutionContext> PRECONDITION = Preconditions.or(
             PATTERNS.stream().map(UsesMethod::new).toArray(UsesMethod[]::new)
     );
     private static final List<MethodMatcher> METHOD_MATCHERS = PATTERNS.stream().map(MethodMatcher::new).collect(Collectors.toList());
@@ -54,54 +59,53 @@ public class RemoveToStringCallsFromArrayInstances extends Recipe {
     @Override
     public String getDescription() {
         return "The result from `toString()` calls on arrays is largely useless. The output does not actually reflect" +
-                " the contents of the array. `Arrays.toString(array)` give the contents of the array.";
+               " the contents of the array. `Arrays.toString(array)` give the contents of the array.";
     }
 
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return Preconditions.check(PRECONDITION, new RemoveToStringFromArraysVisitor());
+        return new RemoveToStringFromArraysVisitor();
+// FIXME not sure we actually need a precondition here, but it currently doesn't cover OBJECT_TOSTRING_MATCHER and OBJECTS_TOSTRING_MATCHER
+//        return Preconditions.check(PRECONDITION, new RemoveToStringFromArraysVisitor());
     }
 
-    private static class RemoveToStringFromArraysVisitor extends JavaIsoVisitor<ExecutionContext> {
+    private static class RemoveToStringFromArraysVisitor extends JavaVisitor<ExecutionContext> {
         @Override
-        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation mi, ExecutionContext ctx) {
-            J.MethodInvocation m = super.visitMethodInvocation(mi, ctx);
-
-            if (OBJECT_TOSTRING_MATCHER.matches(m) || OBJECTS_TOSTRING_MATCHER.matches(m)) {
-                Expression select = m.getSelect();
+        public J visitMethodInvocation(J.MethodInvocation mi, ExecutionContext ctx) {
+            if (OBJECT_TOSTRING_MATCHER.matches(mi) || OBJECTS_TOSTRING_MATCHER.matches(mi)) {
+                Expression select = mi.getSelect();
                 assert select != null;
 
                 if (!(select.getType() instanceof JavaType.Array)) {
-                    return m;
+                    return mi;
                 }
 
                 J.MethodInvocation newInv = JavaTemplate.builder("Arrays.toString(#{anyArray(java.lang.Object)})")
                         .imports("java.util.Arrays")
                         .build()
-                        .apply(getCursor(), m.getCoordinates().replace(), select);
+                        .apply(getCursor(), mi.getCoordinates().replace(), select);
                 maybeAddImport("java.util.Arrays");
                 return newInv;
-            }else if (METHOD_MATCHERS.stream().anyMatch(matcher -> matcher.matches(m))) {
+            } else if (METHOD_MATCHERS.stream().anyMatch(matcher -> matcher.matches(mi))) {
                 // deals with edge cases where .toString() is called implicitly
-                List<Expression> arguments = m.getArguments();
+                List<Expression> arguments = mi.getArguments();
                 for (Expression arg : arguments) {
                     if (arg.getType() instanceof JavaType.Array) {
                         Cursor c = getCursor();
-                        c.putMessage("METHOD_KEY", m);
+                        c.putMessage("METHOD_KEY", mi);
                         break;
                     }
                 }
             }
 
-            return m;
+            return super.visitMethodInvocation(mi, ctx);
         }
 
         @Override
         public Expression visitExpression(Expression exp, ExecutionContext ctx) {
-            Expression e = super.visitExpression(exp, ctx);
-            Cursor c = getCursor().dropParentWhile(J.Parentheses.class::isInstance);
-            System.out.println(c.getNearestMessage("METHOD_KEY") != null);
-            if (c.getMessage("METHOD_KEY") != null) {
-                if (e.getType() instanceof JavaType.Array) {
+            Expression e = (Expression) super.visitExpression(exp, ctx);
+            if (e.getType() instanceof JavaType.Array) {
+                Cursor c = getCursor().dropParentWhile(is -> is instanceof J.Parentheses || !(is instanceof Tree));
+                if (c.getMessage("METHOD_KEY") != null) {
                     Expression newExpression = JavaTemplate.builder("Arrays.toString(#{anyArray(java.lang.Object)})")
                             .imports("java.util.Arrays")
                             .build()
