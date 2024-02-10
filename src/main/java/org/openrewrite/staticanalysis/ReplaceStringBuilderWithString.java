75b6400ed8159823c9116605299f751960efcfc5
Tim te Beek <tim@moderne.io>
2024-02-10 17:45:48 +0100

    Update ReplaceStringBuilderWithString.java

--- src/main/java/org/openrewrite/staticanalysis/ReplaceStringBuilderWithString.java
+++ src/main/java/org/openrewrite/staticanalysis/ReplaceStringBuilderWithString.java
@@ -16,8 +16,10 @@
 package org.openrewrite.staticanalysis;
 
 import org.openrewrite.*;
+import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.PartProvider;
 import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.tree.*;
 import org.openrewrite.marker.Markers;
@@ -32,8 +34,6 @@ import static org.openrewrite.Tree.randomId;
 public class ReplaceStringBuilderWithString extends Recipe {
     private static final MethodMatcher STRING_BUILDER_APPEND = new MethodMatcher("java.lang.StringBuilder append(..)");
     private static final MethodMatcher STRING_BUILDER_TO_STRING = new MethodMatcher("java.lang.StringBuilder toString()");
-    private static J.Parentheses parenthesesTemplate;
-    private static J.MethodInvocation stringValueOfTemplate;
 
     @Override
     public String getDisplayName() {
@@ -54,139 +54,158 @@ public class ReplaceStringBuilderWithString extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return Preconditions.check(Preconditions.and(new UsesMethod<>(STRING_BUILDER_APPEND), new UsesMethod<>(STRING_BUILDER_TO_STRING)), new JavaVisitor<ExecutionContext>() {
-            @Override
-            public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
-                J.MethodInvocation m = (J.MethodInvocation) super.visitMethodInvocation(method, ctx);
+        return Preconditions.check(
+                Preconditions.and(
+                        new UsesMethod<>(STRING_BUILDER_APPEND),
+                        new UsesMethod<>(STRING_BUILDER_TO_STRING)),
+                new StringBuilderToAppendVisitor()
+        );
+    }
 
-                if (STRING_BUILDER_TO_STRING.matches(method)) {
-                    List<Expression> methodCallsChain = new ArrayList<>();
-                    List<Expression> arguments = new ArrayList<>();
-                    boolean isFlattenable = flatMethodInvocationChain(method, methodCallsChain, arguments);
-                    if (!isFlattenable) {
-                        return m;
-                    }
+    private static class StringBuilderToAppendVisitor extends JavaVisitor<ExecutionContext> {
+        @Deprecated
+        private static J.Parentheses parenthesesTemplate;
+        @Deprecated
+        private static J.MethodInvocation stringValueOfTemplate;
 
-                    Collections.reverse(arguments);
-                    adjustExpressions(arguments);
-                    if (arguments.isEmpty()) {
-                        return m;
-                    }
+        @Override
+        public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+            J.MethodInvocation m = (J.MethodInvocation) super.visitMethodInvocation(method, ctx);
 
-                    Expression additive = ChainStringBuilderAppendCalls.additiveExpression(arguments)
-                            .withPrefix(method.getPrefix());
-
-                    if (isAMethodSelect(method)) {
-                        additive = wrapExpression(additive);
-                    }
-
-                    return additive;
+            if (STRING_BUILDER_TO_STRING.matches(method)) {
+                List<Expression> methodCallsChain = new ArrayList<>();
+                List<Expression> arguments = new ArrayList<>();
+                boolean isFlattenable = flatMethodInvocationChain(method, methodCallsChain, arguments);
+                if (!isFlattenable) {
+                    return m;
                 }
-                return m;
+
+                Collections.reverse(arguments);
+                adjustExpressions(arguments);
+                if (arguments.isEmpty()) {
+                    return m;
+                }
+
+                Expression additive = ChainStringBuilderAppendCalls.additiveExpression(arguments)
+                        .withPrefix(method.getPrefix());
+
+                if (isAMethodSelect(method)) {
+                    additive = JavaTemplate.builder("(#{any()})").build().apply(getCursor(), m.getCoordinates().replace(), additive);
+                }
+
+                return additive;
+            }
+            return m;
+        }
+
+        // Check if a method call is a select of another method call
+        private boolean isAMethodSelect(J.MethodInvocation method) {
+            Cursor parent = getCursor().getParent(2); // 2 means skip right padded cursor
+            if (parent == null || !(parent.getValue() instanceof J.MethodInvocation)) {
+                return false;
+            }
+            return ((J.MethodInvocation) parent.getValue()).getSelect() == method;
+        }
+
+        private J.Literal toStringLiteral(J.Literal input) {
+            if (input.getType() == JavaType.Primitive.String) {
+                return input;
             }
 
-            // Check if a method call is a select of another method call
-            private boolean isAMethodSelect(J.MethodInvocation method) {
-                Cursor parent = getCursor().getParent(2); // 2 means skip right padded cursor
-                if (parent == null || !(parent.getValue() instanceof J.MethodInvocation)) {
+            String value = input.getValueSource();
+            return new J.Literal(randomId(), Space.EMPTY, Markers.EMPTY, value,
+                    "\"" + value + "\"", null, JavaType.Primitive.String);
+        }
+
+        private void adjustExpressions(List<Expression> arguments) {
+            for (int i = 0; i < arguments.size(); i++) {
+                if (i == 0) {
+                    // the first expression must be a String type to support case like `new StringBuilder().append(1)`
+                    if (!TypeUtils.isString(arguments.get(0).getType())) {
+                        if (arguments.get(0) instanceof J.Literal) {
+                            // wrap by ""
+                            arguments.set(0, toStringLiteral((J.Literal) arguments.get(0)));
+                        } else {
+                            J.MethodInvocation stringValueOf = getStringValueOfMethodInvocationTemplate()
+                                    .withArguments(Collections.singletonList(arguments.get(0)))
+                                    .withPrefix(arguments.get(0).getPrefix());
+                            arguments.set(0, stringValueOf);
+                        }
+                    }
+                } else {
+                    // wrap by parentheses to support case like `.append(1+2)`
+                    Expression arg = arguments.get(i);
+                    if (!(arg instanceof J.Identifier || arg instanceof J.Literal || arg instanceof J.MethodInvocation)) {
+                        arguments.set(i, wrapExpression(arg));
+                    }
+                }
+            }
+        }
+
+        /**
+         * Return true if the method calls chain is like "new StringBuilder().append("A")....append("B");"
+         *
+         * @param method      a StringBuilder.toString() method call
+         * @param methodChain output methods chain
+         * @param arguments   output expression list to be chained by '+'.
+         */
+        private boolean flatMethodInvocationChain(J.MethodInvocation method,
+                                                  List<Expression> methodChain,
+                                                  List<Expression> arguments) {
+            Expression select = method.getSelect();
+            while (select != null) {
+                methodChain.add(select);
+                if (!(select instanceof J.MethodInvocation)) {
+                    break;
+                }
+
+                J.MethodInvocation selectMethod = (J.MethodInvocation) select;
+                select = selectMethod.getSelect();
+
+                if (!STRING_BUILDER_APPEND.matches(selectMethod)) {
                     return false;
                 }
-                return ((J.MethodInvocation) parent.getValue()).getSelect() == method;
-            }
-        });
-    }
 
-    private J.Literal toStringLiteral(J.Literal input) {
-        if (input.getType() == JavaType.Primitive.String) {
-            return input;
-        }
-
-        String value = input.getValueSource();
-        return new J.Literal(randomId(), Space.EMPTY, Markers.EMPTY, value,
-                "\"" + value + "\"", null, JavaType.Primitive.String);
-    }
-
-    private void adjustExpressions(List<Expression> arguments) {
-        for (int i = 0; i < arguments.size(); i++) {
-            if (i == 0) {
-                // the first expression must be a String type to support case like `new StringBuilder().append(1)`
-                if (!TypeUtils.isString(arguments.get(0).getType())) {
-                    if (arguments.get(0) instanceof J.Literal) {
-                        // wrap by ""
-                        arguments.set(0, toStringLiteral((J.Literal) arguments.get(0)));
-                    } else {
-                        JavaTemplate stringValueOf =
-                            JavaTemplate.builder("String.valueOf(1 + 2)")
-                                .build();
-                        
-                        arguments.set(0, stringValueOf);
-                    }
-                }
-            } else {
-                // wrap by parentheses to support case like `.append(1+2)`
-                Expression arg = arguments.get(i);
-                if (!(arg instanceof J.Identifier || arg instanceof J.Literal || arg instanceof J.MethodInvocation)) {
-                    arguments.set(i, wrapExpression(arg));
+                List<Expression> args = selectMethod.getArguments();
+                if (args.size() != 1) {
+                    return false;
+                } else {
+                    arguments.add(args.get(0));
                 }
             }
-        }
-    }
 
-    /**
-     * Return true if the method calls chain is like "new StringBuilder().append("A")....append("B");"
-     *
-     * @param method      a StringBuilder.toString() method call
-     * @param methodChain output methods chain
-     * @param arguments   output expression list to be chained by '+'.
-     */
-    private static boolean flatMethodInvocationChain(J.MethodInvocation method,
-                                                     List<Expression> methodChain,
-                                                     List<Expression> arguments
-    ) {
-        Expression select = method.getSelect();
-        while (select != null) {
-            methodChain.add(select);
-            if (!(select instanceof J.MethodInvocation)) {
-                break;
+            if (select instanceof J.NewClass &&
+                ((J.NewClass) select).getClazz() != null &&
+                TypeUtils.isOfClassType(((J.NewClass) select).getClazz().getType(), "java.lang.StringBuilder")) {
+                J.NewClass nc = (J.NewClass) select;
+                if (nc.getArguments().size() == 1 && TypeUtils.isString(nc.getArguments().get(0).getType())) {
+                    arguments.add(nc.getArguments().get(0));
+                }
+                return true;
             }
-
-            J.MethodInvocation selectMethod = (J.MethodInvocation) select;
-            select = selectMethod.getSelect();
-
-            if (!STRING_BUILDER_APPEND.matches(selectMethod)) {
-                return false;
-            }
-
-            List<Expression> args = selectMethod.getArguments();
-            if (args.size() != 1) {
-                return false;
-            } else {
-                arguments.add(args.get(0));
-            }
+            return false;
         }
 
-        if (select instanceof J.NewClass &&
-            ((J.NewClass) select).getClazz() != null &&
-            TypeUtils.isOfClassType(((J.NewClass) select).getClazz().getType(), "java.lang.StringBuilder")) {
-            J.NewClass nc = (J.NewClass) select;
-            if (nc.getArguments().size() == 1 && TypeUtils.isString(nc.getArguments().get(0).getType())) {
-                arguments.add(nc.getArguments().get(0));
+        @Deprecated
+        private J.MethodInvocation getStringValueOfMethodInvocationTemplate() {
+            if (stringValueOfTemplate == null) {
+                stringValueOfTemplate = PartProvider.buildPart("class C {\n" +
+                                                               "    void foo() {\n" +
+                                                               "        Object obj = 1 + 2;\n" +
+                                                               "        String.valueOf(obj);\n" +
+                                                               "    }\n" +
+                                                               "}",
+                        J.MethodInvocation.class);
             }
-            return true;
+            return stringValueOfTemplate;
         }
-        return false;
-    }
 
-    public static J.Parentheses getParenthesesTemplate() {
-        if (parenthesesTemplate == null) {
-            parenthesesTemplate = PartProvider.buildPart("class B { void foo() { (\"A\" + \"B\").length(); } } ", J.Parentheses.class); // TODO: Needs to be updated to new logic ...
+        @Deprecated
+        private <T extends J> J.Parentheses<T> wrapExpression(Expression exp) {
+            if (parenthesesTemplate == null) {
+                parenthesesTemplate = PartProvider.buildPart("class B { void foo() { (\"A\" + \"B\").length(); } } ", J.Parentheses.class);
+            }
+            return parenthesesTemplate.withTree(exp).withPrefix(exp.getPrefix());
         }
-        return parenthesesTemplate;
-    }
-
-    
-
-    public static <T extends J> J.Parentheses<T> wrapExpression(Expression exp) {
-        return getParenthesesTemplate().withTree(exp).withPrefix(exp.getPrefix());
     }
 }
