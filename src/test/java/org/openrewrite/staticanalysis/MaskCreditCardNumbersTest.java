1826c680d5c7ab76712cd1c7b3cde9b0948b4231
Sam Snyder <sam@moderne.io>
2024-06-27 15:32:40 -0700

    Add recipe which masks the back half of string literals which look like credit card numbers with "X"

---
+++ src/test/java/org/openrewrite/staticanalysis/MaskCreditCardNumbersTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+
+class MaskCreditCardNumbersTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new MaskCreditCardNumbers());
+    }
+
+    @Test
+    void noSpaces() {
+        rewriteRun(
+          //language=java
+          java("""
+          class A {
+              String cc = "1234567890123456";
+          }
+          """,
+            """
+            class A {
+                String cc = "12345678XXXXXXXX";
+            }
+            """)
+        );
+    }
+
+    @Test
+    void withSpaces() {
+        rewriteRun(
+          //language=java
+          java("""
+          class A {
+              String cc = "1234 5678 9012 3456";
+          }
+          """,
+          """
+          class A {
+              String cc = "1234 5678 XXXX XXXX";
+          }
+          """)
+        );
+    }
+}
