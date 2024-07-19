aa561cdfa2065bfbd6c04bcf87052cc039e4b83a
Tim te Beek <timtebeek@gmail.com>
2024-07-19 12:53:52 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/staticanalysis/NullableOnMethodReturnTypeTest.java
+++ src/test/java/org/openrewrite/staticanalysis/NullableOnMethodReturnTypeTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.staticanalysis;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -28,6 +29,7 @@ class NullableOnMethodReturnTypeTest implements RewriteTest {
         spec.recipe(new NullableOnMethodReturnType());
     }
 
+    @DocumentExample
     @Test
     void nullableOnMethodReturnType() {
         rewriteRun(
