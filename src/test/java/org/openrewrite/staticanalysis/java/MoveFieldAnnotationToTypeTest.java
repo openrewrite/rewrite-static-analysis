717fbb193659a655f62ae569cf768d9ea20c4c5c
Tim te Beek <timtebeek@gmail.com>
2024-08-21 07:25:18 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/staticanalysis/java/MoveFieldAnnotationToTypeTest.java
+++ src/test/java/org/openrewrite/staticanalysis/java/MoveFieldAnnotationToTypeTest.java
@@ -23,7 +23,7 @@ import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.java;
 
 @SuppressWarnings("deprecation")
-public class MoveFieldAnnotationToTypeTest implements RewriteTest {
+class MoveFieldAnnotationToTypeTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
