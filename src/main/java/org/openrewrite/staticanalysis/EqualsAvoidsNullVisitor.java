f2b94d49dce53900a9468d8bc3e7b2aaf044f265
I753089 <vincent.potucek@sap.com>
2024-10-14 16:20:21 +0200

    condense

--- src/main/java/org/openrewrite/staticanalysis/EqualsAvoidsNullVisitor.java
+++ src/main/java/org/openrewrite/staticanalysis/EqualsAvoidsNullVisitor.java
@@ -48,19 +48,18 @@ public class EqualsAvoidsNullVisitor<P> extends JavaVisitor<P> {
     @Override
     public J visitMethodInvocation(J.MethodInvocation method, P p) {
         J.MethodInvocation methodInvocation = (J.MethodInvocation) super.visitMethodInvocation(method, p);
-        if (isNull(methodInvocation.getSelect())) {
-            return methodInvocation;
-        }
-        return !(methodInvocation.getSelect() instanceof J.Literal)
-                && methodInvocation.getArguments().get(0) instanceof J.Literal
-                && (EQUALS.matches(methodInvocation)
-                || !style.getIgnoreEqualsIgnoreCase()
-                && EQUALS_IGNORE_CASE.matches(methodInvocation)
-                || COMPARE_TO.matches(methodInvocation)
-                || COMPARE_TO_IGNORE_CASE.matches(methodInvocation)
-                || CONTENT_EQUALS.matches(methodInvocation))
-                ? visitMethodInvocation(methodInvocation)
-                : methodInvocation;
+        return isNull(methodInvocation.getSelect()) ?
+                methodInvocation :
+                !(methodInvocation.getSelect() instanceof J.Literal)
+                        && methodInvocation.getArguments().get(0) instanceof J.Literal
+                        && (EQUALS.matches(methodInvocation)
+                        || !style.getIgnoreEqualsIgnoreCase()
+                        && EQUALS_IGNORE_CASE.matches(methodInvocation)
+                        || COMPARE_TO.matches(methodInvocation)
+                        || COMPARE_TO_IGNORE_CASE.matches(methodInvocation)
+                        || CONTENT_EQUALS.matches(methodInvocation))
+                        ? visitMethodInvocation(methodInvocation)
+                        : methodInvocation;
     }
 
 
