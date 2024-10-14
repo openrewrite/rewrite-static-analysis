129674eca7b5dc6b4b6f8c6e7b73add4e8c8e100
I753089 <vincent.potucek@sap.com>
2024-10-14 14:26:59 +0200

    EqualsAvoidsNullTest

--- src/test/java/org/openrewrite/staticanalysis/EqualsAvoidsNullTest.java
+++ src/test/java/org/openrewrite/staticanalysis/EqualsAvoidsNullTest.java
@@ -23,7 +23,7 @@ import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.java;
 
 @SuppressWarnings({"ClassInitializerMayBeStatic", "StatementWithEmptyBody", "ConstantConditions"})
-class EqualsAvoidsNullVisitorTest implements RewriteTest {
+class EqualsAvoidsNullTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
