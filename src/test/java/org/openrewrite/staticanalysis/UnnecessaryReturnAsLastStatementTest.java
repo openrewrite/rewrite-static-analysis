148e4f484775f9cd095e75988a274f0859f09b5b
Greg Oledzki <762437+mccartney@users.noreply.github.com>
2024-11-17 17:01:16 +0100

    remove public in test
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/staticanalysis/UnnecessaryReturnAsLastStatementTest.java
+++ src/test/java/org/openrewrite/staticanalysis/UnnecessaryReturnAsLastStatementTest.java
@@ -22,7 +22,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class UnnecessaryReturnAsLastStatementTest implements RewriteTest {
+class UnnecessaryReturnAsLastStatementTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipe(new UnnecessaryReturnAsLastStatement());
