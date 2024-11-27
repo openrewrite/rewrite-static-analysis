a8ceb20f83bad4610b2d73c3018be34f33d81e5d
Tim te Beek <tim@moderne.io>
2024-11-27 14:51:07 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/staticanalysis/nameconvention/NameConventionFactory.java
+++ src/main/java/org/openrewrite/staticanalysis/nameconvention/NameConventionFactory.java
@@ -1,4 +1,19 @@
-package org.openrewrite.staticanalysis.nameconvention;
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.jspecify.annotations.Nullable;
 
 import fj.data.Java;
 import org.openrewrite.SourceFile;
@@ -12,7 +27,7 @@ public class NameConventionFactory {
 
     static Map<String, NameConvention> conventionMap = new HashMap<>();
 
-    public static NameConvention getNameConvention(SourceFile sourceFile) {
+    public static @Nullable NameConvention getNameConvention(SourceFile sourceFile) {
         if (sourceFile instanceof Cs){
             conventionMap.computeIfAbsent("csharp", k -> new CsharpNameConvention());
             return conventionMap.get("csharp");
