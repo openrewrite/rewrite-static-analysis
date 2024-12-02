d5cd0cd39172ad68a2fbab76df5645c2db0200ef
Jacob van Lingen <jacobvanlingen@hotmail.com>
2024-12-02 12:08:25 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/staticanalysis/charp/ExplicitInitializationVisitorCsharpTest.java
+++ src/test/java/org/openrewrite/staticanalysis/charp/ExplicitInitializationVisitorCsharpTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.staticanalysis.charp;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.staticanalysis.ExplicitInitialization;
 import org.openrewrite.test.RecipeSpec;
@@ -46,6 +47,7 @@ class ExplicitInitializationVisitorCsharpTest implements RewriteTest {
           );
     }
 
+    @DocumentExample
     @Test
     void removeExplicitInitialization() {
         rewriteRun(
