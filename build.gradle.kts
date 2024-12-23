14346ee73525ebf6d41e37e12d7509bf369630c7
lingenj <jacob.van.lingen@moderne.io>
2024-12-23 16:44:59 +0100

    Set groovy/kotlin/csharp as provided dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
     id("org.openrewrite.build.moderne-source-available-license") version "latest.release"
+    id("com.netflix.nebula.provided-base") version "10.0.1"
 }
+apply(plugin = "com.netflix.nebula.provided-base")
 
 group = "org.openrewrite.recipe"
 description = "The first Static Analysis and REMEDIATION tool"
@@ -17,22 +19,19 @@ dependencies {
     implementation("org.openrewrite.meta:rewrite-analysis:${rewriteVersion}")
     implementation("org.apache.commons:commons-text:latest.release")
 
-    // Limit transitive dependencies for downstream projects like rewrite-spring
-    compileOnly("org.openrewrite:rewrite-groovy")
-    compileOnly("org.openrewrite:rewrite-kotlin:${rewriteVersion}")
-    compileOnly("org.openrewrite:rewrite-csharp:${rewriteVersion}")
-
     annotationProcessor("org.openrewrite:rewrite-templating:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-templating:${rewriteVersion}")
-    compileOnly("com.google.errorprone:error_prone_core:2.+:with-dependencies") {
+    compileOnly("com.google.errorprone:error_prone_core:2.+") {
         exclude("com.google.auto.service", "auto-service-annotations")
     }
 
-    testImplementation("org.openrewrite:rewrite-groovy")
-    testImplementation("org.openrewrite:rewrite-kotlin:${rewriteVersion}")
-    testImplementation("org.openrewrite:rewrite-csharp:${rewriteVersion}")
-    testImplementation("org.openrewrite:rewrite-test")
+    provided("org.openrewrite:rewrite-groovy")
+    provided("org.openrewrite:rewrite-kotlin:${rewriteVersion}")
+    provided("org.openrewrite:rewrite-csharp:${rewriteVersion}")
+
     testImplementation("org.jetbrains:annotations:24.+")
+    testImplementation("org.openrewrite:rewrite-groovy")
+    testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.junit-pioneer:junit-pioneer:2.+")
     testImplementation("junit:junit:4.13.2")
 
@@ -40,4 +39,10 @@ dependencies {
 
     testRuntimeOnly("org.openrewrite:rewrite-java-17")
     testRuntimeOnly("com.google.code.findbugs:jsr305:latest.release")
+}
+
+tasks.register("listConfigurations") {
+    doLast {
+        configurations.forEach { println(it.name) }
+    }
 }
