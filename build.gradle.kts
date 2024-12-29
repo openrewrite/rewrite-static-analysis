8fcb25c0043b5dabd2f7d98c4c1c39cb974aa0ee
Vincent Potucek <vincent.potucek@sap.com>
2024-12-29 12:49:00 +0100

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
     id("org.openrewrite.build.moderne-source-available-license") version "latest.release"
-    id("org.openrewrite.rewrite") version("6.29.0")
+    id("org.openrewrite.rewrite") version("6.29.2")
 }
 
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 group = "org.openrewrite.recipe"
 description = "The first Static Analysis and REMEDIATION tool"
 
-val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
+    annotationProcessor("org.openrewrite:rewrite-templating:${rewriteVersion}")
+    annotationProcessor("org.projectlombok:lombok:latest.release")
     compileOnly("org.projectlombok:lombok:latest.release")
     compileOnly("com.google.errorprone:error_prone_core:2.+:with-dependencies") {
         exclude("com.google.auto.service", "auto-service-annotations")
     }
-    annotationProcessor("org.openrewrite:rewrite-templating:${rewriteVersion}")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
     implementation("org.apache.commons:commons-text:latest.release")
     implementation("org.openrewrite.meta:rewrite-analysis:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-csharp:${rewriteVersion}")
@@ -23,7 +23,6 @@ dependencies {
     implementation("org.openrewrite:rewrite-kotlin:${rewriteVersion}")
     implementation("org.openrewrite:rewrite-templating:${rewriteVersion}")
     implementation(platform("org.openrewrite:rewrite-bom:${rewriteVersion}"))
-    rewrite("org.openrewrite.recipe:rewrite-static-analysis:1.22.0")
     testImplementation("com.google.code.gson:gson:latest.release")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains:annotations:24.+")
@@ -33,9 +32,14 @@ dependencies {
     testImplementation("org.projectlombok:lombok:latest.release")
     testRuntimeOnly("com.google.code.findbugs:jsr305:latest.release")
     testRuntimeOnly("org.openrewrite:rewrite-java-17")
+    rewrite("org.openrewrite.recipe:rewrite-static-analysis:1.22.0")
 }
-
+repositories {
+    mavenCentral()
+}
 rewrite {
     activeRecipe("org.openrewrite.staticanalysis.CommonStaticAnalysis")
+//    activeRecipe("org.openrewrite.staticanalysis.EqualsAvoidsNull") // not found
+//    activeRecipe("org.openrewrite.java.RemoveObjectsIsNull") // found
     setExportDatatables(true)
 }
