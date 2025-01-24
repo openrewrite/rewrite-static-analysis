53b444d8ee1fb7b8d348adced5b1006998d1636b
Karsten Thoms <karsten.thoms@gmail.com>
2025-01-24 12:15:31 +0100

    recipe: On abstract classes make constructors protected (#450)
    * recipe: On abstract classes make constructors protected
    
    This recipe resolves Sonar issues of type S5993: Constructors of an "abstract" class should not be declared "public"
    
    * Initial polish and new failing test
    
    * Fix failing test & use markdown in name & description
    
    * Include recipe with `CommonStaticAnalysis` going forward
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/staticanalysis/AbstractClassPublicConstructorTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+@SuppressWarnings("java:S2699")
+class AbstractClassPublicConstructorTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new AbstractClassPublicConstructor());
+    }
+
+    @DocumentExample
+    @Test
+    void replacePublicByProtected() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+            abstract class Test {
+                public Test() {
+                }
+            }
+            """,
+            """
+            abstract class Test {
+                protected Test() {
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void noReplaceOnNonAbstractClass() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+            class Test {
+                public Test() {
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void noReplaceOnPackageProtectedConstructor() {
+        rewriteRun(
+          //language=java
+          java("""
+            abstract class Test {
+                Test() {
+                }
+            }
+            """
+          )
+        );
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite-static-analysis/issues/449")
+    @Test
+    void noReplaceOnNestedClasses() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+            abstract class Test {
+                static class Nested {
+                    public Nested() {
+                    }
+                }
+            }
+            """
+          )
+        );
+    }
+}
