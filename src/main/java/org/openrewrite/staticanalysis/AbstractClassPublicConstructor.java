58a478bad20b512e9f17069d29e8dad4e16882b6
Tim te Beek <tim@moderne.io>
2025-01-24 11:56:44 +0100

    Initial polish and new failing test

--- src/main/java/org/openrewrite/staticanalysis/AbstractClassPublicConstructor.java
+++ src/main/java/org/openrewrite/staticanalysis/AbstractClassPublicConstructor.java
@@ -1,7 +1,19 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.staticanalysis;
-
-import java.util.Collections;
-import java.util.Set;
 
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
@@ -10,46 +22,51 @@ import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
 
+import java.util.Collections;
+import java.util.Set;
+
+import static org.openrewrite.java.tree.J.Modifier.Type.*;
+
 public class AbstractClassPublicConstructor extends Recipe {
-  @Override
-  public String getDisplayName() {
-    return "Make constructors of abstract classes protected";
-  }
-
-  @Override
-  public String getDescription() {
-    return "Constructors of abstract classes can only be called in constructors of their subclasses. "
-      + "Therefore the visibility of public constructors are reduced to protected.";
-  }
-
-  @Override
-  public Set<String> getTags() {
-    return Collections.singleton("RSPEC-S5993");
-  }
-
-  @Override
-  public TreeVisitor<?, ExecutionContext> getVisitor() {
-    return new JavaIsoVisitor<ExecutionContext>() {
-      @Override
-      public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-        J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
-        if (cd.getModifiers().stream().anyMatch(mod -> mod.getType() == J.Modifier.Type.Abstract)) {
-          doAfterVisit(CHANGE_CONSTRUCTOR_ACCESS_LEVEL_VISITOR);
-        }
-        return cd;
-      }
-    };
-  }
-
-  static final TreeVisitor<?, ExecutionContext> CHANGE_CONSTRUCTOR_ACCESS_LEVEL_VISITOR = new JavaIsoVisitor<ExecutionContext>() {
     @Override
-    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-      J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
-      if (md.isConstructor() && md.getModifiers().stream().anyMatch(mod -> mod.getType() == J.Modifier.Type.Public)) {
-        md = md.withModifiers(ListUtils.map(md.getModifiers(),
-          mod -> mod.getType() == J.Modifier.Type.Public ? mod.withType(J.Modifier.Type.Protected) : mod));
-      }
-      return md;
+    public String getDisplayName() {
+        return "Make constructors of abstract classes protected";
     }
-  };
+
+    @Override
+    public String getDescription() {
+        return "Constructors of abstract classes can only be called in constructors of their subclasses. "
+                + "Therefore the visibility of public constructors are reduced to protected.";
+    }
+
+    @Override
+    public Set<String> getTags() {
+        return Collections.singleton("RSPEC-S5993");
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                J.ClassDeclaration cd = super.visitClassDeclaration(classDecl, ctx);
+                if (cd.hasModifier(Abstract)) {
+                    doAfterVisit(CHANGE_CONSTRUCTOR_ACCESS_LEVEL_VISITOR);
+                }
+                return cd;
+            }
+        };
+    }
+
+    static final TreeVisitor<?, ExecutionContext> CHANGE_CONSTRUCTOR_ACCESS_LEVEL_VISITOR = new JavaIsoVisitor<ExecutionContext>() {
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+            J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
+            if (md.isConstructor() && md.hasModifier(Public)) {
+                md = md.withModifiers(ListUtils.map(md.getModifiers(),
+                        mod -> mod.getType() == Public ? mod.withType(Protected) : mod));
+            }
+            return md;
+        }
+    };
 }
--- src/test/java/org/openrewrite/staticanalysis/AbstractClassPublicConstructorTest.java
+++ src/test/java/org/openrewrite/staticanalysis/AbstractClassPublicConstructorTest.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.staticanalysis;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -20,29 +36,36 @@ class AbstractClassPublicConstructorTest implements RewriteTest {
     void replacePublicByProtected() {
         rewriteRun(
           //language=java
-          java("""
+          java(
+            """
             abstract class Test {
                 public Test() {
                 }
             }
-            """, """
+            """,
+            """
             abstract class Test {
                 protected Test() {
                 }
             }
-            """));
+            """
+          )
+        );
     }
 
     @Test
     void noReplaceOnNonAbstractClass() {
         rewriteRun(
           //language=java
-          java("""
+          java(
+            """
             class Test {
                 public Test() {
                 }
             }
-            """));
+            """
+          )
+        );
     }
 
     @Test
@@ -54,7 +77,26 @@ class AbstractClassPublicConstructorTest implements RewriteTest {
                 Test() {
                 }
             }
-            """));
+            """
+          )
+        );
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite-static-analysis/issues/449")
+    @Test
+    void noReplaceOnNestedClasses() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+            abstract class Test {
+                static class Nested {
+                    public Nested() {
+                    }
+                }
+            }
+            """
+          )
+        );
     }
 }
-
