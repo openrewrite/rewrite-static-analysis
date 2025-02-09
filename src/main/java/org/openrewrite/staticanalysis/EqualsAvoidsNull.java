46f143bf21fa83387c4c2e378a8374a676efbf8d
Vincent Potucek <vincent.potucek@udg.de>
2025-02-09 16:14:59 +0100

    Revert "form"
    This reverts commit 74cd190689e6be4326015f2e9456765139240c86.

--- src/main/java/org/openrewrite/staticanalysis/EqualsAvoidsNull.java
+++ src/main/java/org/openrewrite/staticanalysis/EqualsAvoidsNull.java
@@ -54,10 +54,7 @@ public class EqualsAvoidsNull extends Recipe {
 
     @Override
     public String getDescription() {
-        return """
-                Checks that any combination of String literals is on the left side of an `equals()` comparison.
-                Also checks for String literals assigned to some field (such as `someString.equals(anotherString = "text"))`.
-                """;
+        return "Checks that any combination of String literals is on the left side of an `equals()` comparison. Also checks for String literals assigned to some field (such as `someString.equals(anotherString = \"text\"))`.";
     }
 
     @Override
