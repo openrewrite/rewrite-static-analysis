7026a24c137c60cf4054c8e6873fedb39b1bb321
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-02-11 14:15:30 +0100

    Add Java 21 to classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     testImplementation("com.google.code.gson:gson:latest.release")
 
     testRuntimeOnly("org.openrewrite:rewrite-java-17")
+    testRuntimeOnly("org.openrewrite:rewrite-java-21")
     testRuntimeOnly("com.google.code.findbugs:jsr305:latest.release")
 }
 
