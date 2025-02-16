05932329a6b4067a9de571bdad32ec7f37d10421
Tim te Beek <timtebeek@gmail.com>
2025-02-16 21:29:26 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/staticanalysis/ReplaceArraysAsListWithListOf.java
+++ src/main/java/org/openrewrite/staticanalysis/ReplaceArraysAsListWithListOf.java
@@ -15,8 +15,6 @@
  */
 package org.openrewrite.staticanalysis;
 
-import java.time.Duration;
-
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
@@ -26,6 +24,8 @@ import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesMethod;
 import org.openrewrite.java.tree.J;
+
+import java.time.Duration;
 
 public class ReplaceArraysAsListWithListOf extends Recipe {
 
