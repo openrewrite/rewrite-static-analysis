db096608a3231cc8d8f700eb85590ab1f49f6bdb
JohannisK <johan.kragt@moderne.io>
2025-04-18 10:03:17 +0200

    Update src/main/java/org/openrewrite/staticanalysis/DefaultComesLastVisitor.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/staticanalysis/DefaultComesLastVisitor.java
+++ src/main/java/org/openrewrite/staticanalysis/DefaultComesLastVisitor.java
@@ -17,7 +17,6 @@ package org.openrewrite.staticanalysis;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
-import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.Tree;
 import org.openrewrite.internal.ListUtils;
