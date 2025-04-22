c0b39c0f4393e8d2dbc1fdbc4694e54f337a0cd0
Jammy Louie <jammy@moderne.io>
2025-04-22 14:48:04 -0400

    test: Add test for removing explicit initialization in anonymous subclasses

--- src/test/java/org/openrewrite/staticanalysis/ExplicitInitializationTest.java
+++ src/test/java/org/openrewrite/staticanalysis/ExplicitInitializationTest.java
@@ -195,4 +195,62 @@ class ExplicitInitializationTest implements RewriteTest {
           )
         );
     }
+
+    @Issue("https://github.com/openrewrite/rewrite-static-analysis/issues/109")
+    @Test
+    void removeExplicitInitializationInAnonymousSubClasses() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class Test {
+                  Object o = new Object() {
+                      private int a = 0;
+                      private long b = 0L;
+                      private short c = 0;
+                      private int d = 1;
+                      private long e = 2L;
+                      private int f;
+                      private char g = '\\0';
+
+                      private boolean h = false;
+                      private boolean i = true;
+
+                      private Object j = new Object();
+                      private Object k = null;
+
+                      int[] l = null;
+                      int[] m = new int[0];
+
+                      private final Long n = null;
+                  };
+              }
+              """,
+            """
+              class Test {
+                  Object o = new Object() {
+                      private int a;
+                      private long b;
+                      private short c;
+                      private int d = 1;
+                      private long e = 2L;
+                      private int f;
+                      private char g;
+
+                      private boolean h;
+                      private boolean i = true;
+
+                      private Object j = new Object();
+                      private Object k;
+
+                      int[] l;
+                      int[] m = new int[0];
+
+                      private final Long n = null;
+                  };
+              }
+              """
+          )
+        );
+    }
 }
