39fa67d6fc8cc2656794f7c62a5fe5ab758b1846
Deepak <sdeepaksharma15@gmail.com>
2025-06-27 03:54:56 +0530

    added test cases

--- src/test/java/org/openrewrite/staticanalysis/TryWithResourcesTest.java
+++ src/test/java/org/openrewrite/staticanalysis/TryWithResourcesTest.java
@@ -324,4 +324,306 @@ class TryWithResourcesTest implements RewriteTest {
           )
         );
     }
+
+    @Test
+    void multipleVariableDeclarations() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      InputStream in1, in2 = new FileInputStream("file2.txt");
+                      in1 = new FileInputStream("file1.txt");
+                      try {
+                          int data1 = in1.read();
+                          int data2 = in2.read();
+                      } finally {
+                          in1.close();
+                          in2.close();
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      try (InputStream in1 = new FileInputStream("file1.txt");
+                           InputStream in2 = new FileInputStream("file2.txt")) {
+                          int data1 = in1.read();
+                          int data2 = in2.read();
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceClosedInCatchBlock() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() {
+                      InputStream in = null;
+                      try {
+                          in = new FileInputStream("file.txt");
+                          int data = in.read();
+                      } catch (IOException e) {
+                          if (in != null) {
+                              try {
+                                  in.close();
+                              } catch (IOException ignored) {
+                              }
+                          }
+                          throw new RuntimeException(e);
+                      } finally {
+                          if (in != null) {
+                              try {
+                                  in.close();
+                              } catch (IOException ignored) {
+                              }
+                          }
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void qualifiedCloseMethodCall() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  static class Wrapper {
+                      InputStream stream;
+                      Wrapper(InputStream s) { this.stream = s; }
+                      InputStream getStream() { return stream; }
+                  }
+
+                  void method() throws IOException {
+                      Wrapper wrapper = new Wrapper(new FileInputStream("file.txt"));
+                      try {
+                          int data = wrapper.getStream().read();
+                      } finally {
+                          wrapper.getStream().close();
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void nonAutoCloseableResource() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  static class CustomResource {
+                      public void close() {
+                          // Custom close logic
+                      }
+                      public void doSomething() {}
+                  }
+
+                  void method() {
+                      CustomResource resource = new CustomResource();
+                      try {
+                          resource.doSomething();
+                      } finally {
+                          resource.close();
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceAssignedToField() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  private InputStream fieldStream;
+
+                  void method() throws IOException {
+                      fieldStream = new FileInputStream("file.txt");
+                      try {
+                          int data = fieldStream.read();
+                      } finally {
+                          fieldStream.close();
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceWithComplexFinallyLogic() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      InputStream in = new FileInputStream("file.txt");
+                      boolean success = false;
+                      try {
+                          int data = in.read();
+                          success = true;
+                      } finally {
+                          if (success) {
+                              System.out.println("Success!");
+                          } else {
+                              System.out.println("Failed!");
+                          }
+                          if (in != null) {
+                              in.close();
+                          }
+                          System.out.println("Cleanup done");
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      boolean success = false;
+                      try (InputStream in = new FileInputStream("file.txt")) {
+                          int data = in.read();
+                          success = true;
+                      } finally {
+                          if (success) {
+                              System.out.println("Success!");
+                          } else {
+                              System.out.println("Failed!");
+                          }
+                          System.out.println("Cleanup done");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceUsedAfterTryBlock() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      InputStream in = new FileInputStream("file.txt");
+                      try {
+                          int data = in.read();
+                      } finally {
+                          in.close();
+                      }
+                      // Resource is referenced after try - can still use try(in) syntax
+                      System.out.println("Stream was: " + in);
+                  }
+              }
+              """,
+            """
+              import java.io.FileInputStream;
+              import java.io.IOException;
+              import java.io.InputStream;
+
+              class Test {
+                  void method() throws IOException {
+                      InputStream in = new FileInputStream("file.txt");
+                      try (in) {
+                          int data = in.read();
+                      }
+                      // Resource is referenced after try - can still use try(in) syntax
+                      System.out.println("Stream was: " + in);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceReassignedInTryBlock() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  void method() throws IOException {
+                      InputStream in = new FileInputStream("file1.txt");
+                      try {
+                          if (Math.random() > 0.5) {
+                              in = new FileInputStream("file2.txt");
+                          }
+                          int data = in.read();
+                      } finally {
+                          in.close();
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void resourceWithStaticCloseCall() {
+        rewriteRun(
+          java(
+            """
+              import java.io.*;
+
+              class Test {
+                  static void closeQuietly(InputStream stream) {
+                      try {
+                          if (stream != null) {
+                              stream.close();
+                          }
+                      } catch (IOException ignored) {}
+                  }
+
+                  void method() throws IOException {
+                      InputStream in = new FileInputStream("file.txt");
+                      try {
+                          int data = in.read();
+                      } finally {
+                          closeQuietly(in);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
 }
