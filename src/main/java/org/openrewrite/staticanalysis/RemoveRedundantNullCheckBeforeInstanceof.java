72e2edfadb44ea6d6cd9e23cbfbc74659e1fe0a6
Tim te Beek <tim@moderne.io>
2025-07-08 23:18:25 +0200

    Recipe `RemoveRedundantNullCheckBeforeInstanceof` (#632)
    * Initial version of `RemoveRedundantNullCheckBeforeInstanceof`
    
    Fixes https://github.com/openrewrite/rewrite-static-analysis/issues/629
    
    * New iteration also converting chained checks
    
    * Trim dead code paths
    
    * Inline & merge nested ifs

---
+++ src/main/java/org/openrewrite/staticanalysis/RemoveRedundantNullCheckBeforeInstanceof.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.SemanticallyEqual;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Set;
+
+@EqualsAndHashCode(callSuper = false)
+@Value
+public class RemoveRedundantNullCheckBeforeInstanceof extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Remove redundant null checks before instanceof";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Removes redundant null checks before instanceof operations since instanceof returns false for null.";
+    }
+
+    @Override
+    public Set<String> getTags() {
+        return Collections.singleton("RSPEC-S1697");
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(1);
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitBinary(J.Binary binary, ExecutionContext ctx) {
+                J.Binary bi = (J.Binary) super.visitBinary(binary, ctx);
+                if (bi.getOperator() != J.Binary.Type.And) {
+                    return bi;
+                }
+
+                Expression left = bi.getLeft();
+                Expression right = bi.getRight();
+
+                // Check if we have a pattern: (expr != null) && (expr instanceof Type)
+                if (left instanceof J.Binary && right instanceof J.InstanceOf) {
+                    J.Binary nullCheck = (J.Binary) left;
+                    J.InstanceOf instanceOf = (J.InstanceOf) right;
+
+                    if (isRedundantNullCheck(nullCheck, instanceOf)) {
+                        // Return just the instanceof check
+                        return instanceOf.withPrefix(bi.getPrefix());
+                    }
+                }
+
+                // Check if we have chained patterns like:
+                // (... && expr != null) && (expr instanceof Type)
+                if (left instanceof J.Binary && ((J.Binary) left).getOperator() == J.Binary.Type.And &&
+                        right instanceof J.InstanceOf) {
+                    J.Binary leftBinary = (J.Binary) left;
+                    J.InstanceOf instanceOf = (J.InstanceOf) right;
+
+                    // Check if the rightmost part of left is a null check for the same expression
+                    Expression rightmostOfLeft = leftBinary.getRight();
+                    if (rightmostOfLeft instanceof J.Binary &&
+                            isRedundantNullCheck((J.Binary) rightmostOfLeft, instanceOf)) {
+                        // Remove the null check from the left side
+                        return bi.withLeft(leftBinary.getLeft()).withRight(instanceOf);
+                    }
+                }
+
+                return bi;
+            }
+
+            private boolean isRedundantNullCheck(J.Binary nullCheck, J.InstanceOf instanceOf) {
+                if (nullCheck.getOperator() == J.Binary.Type.NotEqual) {
+                    if (J.Literal.isLiteralValue(nullCheck.getLeft(), null)) {
+                        return SemanticallyEqual.areEqual(nullCheck.getRight(), instanceOf.getExpression());
+                    }
+                    if (J.Literal.isLiteralValue(nullCheck.getRight(), null)) {
+                        return SemanticallyEqual.areEqual(nullCheck.getLeft(), instanceOf.getExpression());
+                    }
+                }
+                return false;
+            }
+        };
+    }
+}
---
+++ src/test/java/org/openrewrite/staticanalysis/RemoveRedundantNullCheckBeforeInstanceofTest.java
@@ -0,0 +1,424 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+@SuppressWarnings({"ConstantConditions", "ConditionCoveredByFurtherCondition"})
+class RemoveRedundantNullCheckBeforeInstanceofTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new RemoveRedundantNullCheckBeforeInstanceof());
+    }
+
+    @DocumentExample
+    @Test
+    void removeRedundantNullCheck() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s != null && s instanceof String) {
+                          System.out.println("String value: " + s);
+                      }
+                  }
+              }
+              """,
+            """
+              class A {
+                  void foo(String s) {
+                      if (s instanceof String) {
+                          System.out.println("String value: " + s);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeRedundantNullCheckWithNullOnLeft() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object obj) {
+                      if (null != obj && obj instanceof String) {
+                          System.out.println("String value: " + obj);
+                      }
+                  }
+              }
+              """,
+            """
+              class A {
+                  void foo(Object obj) {
+                      if (obj instanceof String) {
+                          System.out.println("String value: " + obj);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+
+    @Test
+    void removeRedundantNullCheckWithMethodInvocation() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo() {
+                      if (getValue() != null && getValue() instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+
+                  String getValue() {
+                      return "test";
+                  }
+              }
+              """,
+            """
+              class A {
+                  void foo() {
+                      if (getValue() instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+
+                  String getValue() {
+                      return "test";
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeRedundantNullCheckWithFieldAccess() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  String field;
+
+                  void foo() {
+                      if (this.field != null && this.field instanceof String) {
+                          System.out.println("String value: " + this.field);
+                      }
+                  }
+              }
+              """,
+            """
+              class A {
+                  String field;
+
+                  void foo() {
+                      if (this.field instanceof String) {
+                          System.out.println("String value: " + this.field);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenDifferentVariables() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s, Object obj) {
+                      if (s != null && obj instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenNotNullCheck() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s == null && s instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenNotInstanceofCheck() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s != null && s.length() > 0) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenOnlyInstanceofCheck() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenOrOperator() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s != null || s instanceof String) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenWrongOrder() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s) {
+                      if (s instanceof String && s != null) {
+                          System.out.println("String value");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeRedundantNullCheckWithTwoVariables() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s, Object obj) {
+                      if (s != null && s instanceof String && obj != null && obj instanceof String) {
+                          System.out.println("Both are strings");
+                      }
+                  }
+              }
+              """,
+            """
+              class A {
+                  void foo(String s, Object obj) {
+                      if (s instanceof String && obj instanceof String) {
+                          System.out.println("Both are strings");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenNullCheckForDifferentVariable() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s, Object obj) {
+                      if (s != null && obj instanceof String) {
+                          System.out.println("Mixed check");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenMultipleInstanceofWithoutMatchingNullChecks() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object a, Object b, Object c) {
+                      if (a instanceof String && b instanceof Integer && c != null) {
+                          System.out.println("Mixed types");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenNullCheckBetweenUnrelatedInstanceof() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object a, Object b, Object c) {
+                      if (a instanceof String && b != null && c instanceof Integer) {
+                          System.out.println("Null check for different variable");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenComplexMixedConditions() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(String s, Object obj, Integer num) {
+                      if (s instanceof String && obj != null && num instanceof Integer && obj.hashCode() > 0) {
+                          System.out.println("Complex condition");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeWhenNullCheckAfterInstanceof() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object a, Object b) {
+                      if (a instanceof String && b instanceof Integer && a != null && b != null) {
+                          System.out.println("Null checks after instanceof");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void partialRemovalWithMixedConditions() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object a, Object b, Object c) {
+                      if (a != null && a instanceof String && b != null && c instanceof Integer) {
+                          System.out.println("Only first null check is redundant");
+                      }
+                  }
+              }
+              """,
+            """
+              class A {
+                  void foo(Object a, Object b, Object c) {
+                      if (a instanceof String && b != null && c instanceof Integer) {
+                          System.out.println("Only first null check is redundant");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeOnlySequentialNullChecks() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class A {
+                  void foo(Object a, Object b) {
+                      if (a != null && b != null && a instanceof String && b instanceof Integer) {
+                          System.out.println("Both null checks are redundant");
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+}
