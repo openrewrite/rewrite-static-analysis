020ea7ecf8915348bb48a937cfd6db20fa750e6c
Tim te Beek <tim@moderne.io>
2025-07-24 02:43:14 +0200

    Update description for `UseDiamondOperator` to escape `<>`

--- src/main/java/org/openrewrite/staticanalysis/UseDiamondOperator.java
+++ src/main/java/org/openrewrite/staticanalysis/UseDiamondOperator.java
@@ -40,7 +40,7 @@ public class UseDiamondOperator extends Recipe {
 
     @Override
     public String getDescription() {
-        return "The diamond operator (`<>`) should be used. Java 7 introduced the diamond operator (<>) to " +
+        return "The diamond operator (`<>`) should be used. Java 7 introduced the diamond operator to " +
                "reduce the verbosity of generics code. For instance, instead of having to declare a `List`'s " +
                "type in both its declaration and its constructor, you can now simplify the constructor declaration " +
                "with `<>`, and the compiler will infer the type.";
