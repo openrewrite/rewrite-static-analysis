6aaa59ef15de91434bc1355860a08ac5b67645c0
Tim te Beek <tim@moderne.io>
2025-07-29 19:12:05 +0200

    Polish MavenJavadocNonAsciiRecipe

--- src/main/java/org/openrewrite/staticanalysis/maven/MavenJavadocNonAsciiRecipe.java
+++ src/main/java/org/openrewrite/staticanalysis/maven/MavenJavadocNonAsciiRecipe.java
@@ -17,27 +17,25 @@ package org.openrewrite.staticanalysis.maven;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.JavaIsoVisitor;
-import org.openrewrite.java.tree.Comment;
-import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.Javadoc;
+import org.openrewrite.java.tree.Space;
 
 import java.text.Normalizer;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
 /**
  * Maven's javadoc-plugin configuration does not support non-ASCII characters in Javadoc comments.
  * This can cause build failures with ambiguous error messages that don't clearly indicate the root cause.
- *
+ * <p>
  * This recipe removes non-ASCII characters from Javadoc comments by:
  * 1. Normalizing text using Unicode NFKD form
  * 2. Removing any characters that are not in the ASCII character set
- *
+ * <p>
  * This is particularly useful when working with international codebases or when comments
  * contain accented characters, special symbols, or other non-ASCII content.
  */
@@ -53,83 +51,32 @@ public class MavenJavadocNonAsciiRecipe extends Recipe {
     @Override
     public String getDescription() {
         return "Maven's javadoc-plugin configuration does not support non-ASCII characters. " +
-               "What makes it tricky is the error is very ambiguous and doesn't help in any way. " +
-               "This recipe removes those non-ASCII characters.";
+                "What makes it tricky is the error is very ambiguous and doesn't help in any way. " +
+                "This recipe removes those non-ASCII characters.";
     }
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new MavenJavadocNonAsciiPruner();
-    }
-
-    public class MavenJavadocNonAsciiPruner extends JavaIsoVisitor<ExecutionContext> {
-
-        @Override
-        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-            return processComments(super.visitClassDeclaration(classDecl, ctx), classDecl.getComments());
-        }
-        @Override
-        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-            return processComments(super.visitMethodDeclaration(method, ctx), method.getComments());
-        }
-        @Override
-        public J.VariableDeclarations.NamedVariable visitVariable(J.VariableDeclarations.NamedVariable variable, ExecutionContext ctx) {
-            return processComments(super.visitVariable(variable, ctx), variable.getComments());
-        }
-
-        private <T extends J> T processComments(T jElement, List<Comment> comments) {
-            List<CommentChangeResult> prunedComments = pruneComments(comments);
-            if (prunedComments.stream().anyMatch(CommentChangeResult::isChanged)) {
-                return jElement.withComments(prunedComments.stream().map(CommentChangeResult::getComment).collect(Collectors.toList()));
-            }
-            return jElement;
-        }
-
-        private List<CommentChangeResult> pruneComments(List<Comment> comments) {
-            return comments.stream()
-                    .map(this::pruneNonAsciiCharacters)
-                    .collect(Collectors.toList());
-        }
-
-        private CommentChangeResult pruneNonAsciiCharacters(Comment comment) {
-            if (comment instanceof Javadoc.DocComment) {
-                Javadoc.DocComment jdc = (Javadoc.DocComment) comment;
-                AtomicBoolean changed = new AtomicBoolean(false);
-                return new CommentChangeResult(jdc.withBody(
-                        jdc.getBody().stream().map(jd -> {
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public Space visitSpace(@Nullable Space space, Space.Location loc, ExecutionContext ctx) {
+                Space sp = super.visitSpace(space, loc, ctx);
+                return sp.withComments(ListUtils.map(sp.getComments(), c -> {
+                    if (c instanceof Javadoc.DocComment) {
+                        Javadoc.DocComment jdc = (Javadoc.DocComment) c;
+                        return jdc.withBody(ListUtils.map(jdc.getBody(), jd -> {
                             if (jd instanceof Javadoc.Text) {
                                 Javadoc.Text jdText = (Javadoc.Text) jd;
-                                String oldText = jdText.getText();
                                 String newText = Normalizer.normalize(jdText.getText(), Normalizer.Form.NFKD)
                                         .replaceAll("[^\\p{ASCII}]", "");
-                                if (!oldText.equals(newText)) {
-                                    changed.set(true);
-                                }
-                                return jdText.withText(newText);
+                                return jdText.getText().equals(newText) ? jd : jdText.withText(newText);
                             }
                             return jd;
-                        }).collect(Collectors.toList())
-                ), changed.get());
+                        }));
+                    }
+                    return c;
+                }));
             }
-            return new CommentChangeResult(comment, false);
-        }
-    }
-
-    public class CommentChangeResult {
-        private final Comment comment;
-        private final boolean changed;
-
-        public CommentChangeResult(Comment comment, boolean changed) {
-            this.comment = comment;
-            this.changed = changed;
-        }
-
-        public Comment getComment() {
-            return comment;
-        }
-
-        public boolean isChanged() {
-            return changed;
-        }
+        };
     }
 }
