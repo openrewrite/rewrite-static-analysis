debf8eb9e64f8a01b32803932f17cb3ab0fae188
Tim te Beek <tim@moderne.io>
2025-08-10 13:41:28 +0200

    Add missing imports

--- src/test/java/org/openrewrite/staticanalysis/SimplifyTernaryTest.java
+++ src/test/java/org/openrewrite/staticanalysis/SimplifyTernaryTest.java
@@ -17,6 +17,7 @@ package org.openrewrite.staticanalysis;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
