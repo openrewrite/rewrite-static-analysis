71ce4aadcb7f0a8a94534373442ddbea77cf8641
Todd Ginsberg <todd@ginsberg.com>
2026-01-02 18:34:49 -0500

    Fix for #800
    - When `w` (week in year) is detected and we are not inside quotes, abandon replacement and return the original `input`.
    - Also simplified `replaceY`, which had a redundant else-if branch.

--- src/main/java/org/openrewrite/staticanalysis/ReplaceWeekYearWithYear.java
+++ src/main/java/org/openrewrite/staticanalysis/ReplaceWeekYearWithYear.java
@@ -106,15 +106,14 @@ public class ReplaceWeekYearWithYear extends Recipe {
 
             for (int i = 0; i < input.length(); i++) {
                 char currentChar = input.charAt(i);
-                char nextChar = (i < input.length() - 1) ? input.charAt(i + 1) : '\0';
 
                 if (currentChar == '\'') {
                     insideQuotes = !insideQuotes;
                     output.append(currentChar);
                 } else if (currentChar == 'Y' && !insideQuotes) {
                     output.append('y');
-                } else if (currentChar == 'Y' && nextChar == '\'') {
-                    output.append(currentChar);
+                } else if (currentChar == 'w' && !insideQuotes) {
+                    return input;
                 } else {
                     output.append(currentChar);
                 }
