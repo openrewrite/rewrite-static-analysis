77719d2d8834629dacdf89eaf66c0b8c6ed47c91
Avimta Likhitkar <132879600+AVIMTA@users.noreply.github.com>
2026-04-11 18:28:00 +0530

    Add space after // in single-line comments (#854)
    * Add recipe to enforce space after // in single-line comments
    
    * fix review comments
    
    * Use ListUtils.map as suggested by reviewer
    
    * fix review comments: use @Getter fields, skip block comments, update copyright year
    
    * Invert instanceof check and combine with isMultiline guard
    
    * Skip noinspection and region/endregion IDE comments
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/staticanalysis/SingleLineCommentSpacingTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import static org.openrewrite.java.Assertions.java;
+import org.openrewrite.test.RewriteTest;
+
+class SingleLineCommentSpacingTest implements RewriteTest {
+
+    @Override
+    public void defaults(org.openrewrite.test.RecipeSpec spec) {
+        spec.recipe(new SingleLineCommentSpacing());
+    }
+
+    @Test
+    void addsSpaceAfterDoubleSlash() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    void method() {
+                        //Hello
+                        int a = 1;
+                    }
+                }
+                """,
+                """
+                class Test {
+                    void method() {
+                        // Hello
+                        int a = 1;
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void doesNotChangeAlreadyCorrectComment() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    void method() {
+                        // Hello
+                        int a = 1;
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void doesNotChangeBlockComment() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    void method() {
+                        /*message*/
+                        int a = 1;
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void doesNotChangeBlockCommentDivider() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    /****divider****/
+                    void method() {
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void skipsLanguageComment() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    void method() {
+                        //language=java
+                        int a = 1;
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void skipsNoinspectionComment() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    @SuppressWarnings("unchecked")
+                    void method() {
+                        //noinspection unchecked
+                        int a = 1;
+                    }
+                }
+                """
+            )
+        );
+    }
+
+    @Test
+    void skipsRegionComments() {
+        rewriteRun(
+            //language=java
+            java(
+                """
+                class Test {
+                    //region MyRegion
+                    void method() {
+                    }
+                    //endregion
+                }
+                """
+            )
+        );
+    }
+}
