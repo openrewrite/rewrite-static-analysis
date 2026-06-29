6736785abae3885345d7a95db5f65059ce856f49
Steve Elliott <steve@moderne.io>
2026-06-29 17:53:00 -0400

    Add RemoveUnreachableMultiCatchAlternative
    Removes `catch` alternatives that are already covered by an earlier
    `catch` clause in the same `try`. When a multi-catch reduces to a
    single alternative it collapses to a regular `catch`; when all
    alternatives are shadowed the entire `catch` clause is dropped.
    
    This pattern most commonly appears after type-substitution migrations
    that cause two `catch` clauses to overlap (for example, renaming an
    exception so its parent and child end up in adjacent catches), which
    is a Java compile error. Intended to run before
    `CombineSemanticallyEqualCatchBlocks` so that collapsed singletons
    become candidates for combination.

---
+++ src/test/java/org/openrewrite/staticanalysis/RemoveUnreachableMultiCatchAlternativeTest.java
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.staticanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class RemoveUnreachableMultiCatchAlternativeTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new RemoveUnreachableMultiCatchAlternative());
+    }
+
+    @DocumentExample
+    @Test
+    void dropAlternativeShadowedByEarlierCatch() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (FileNotFoundException | IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void dropTrailingAlternativeShadowedByEarlierCatch() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException | FileNotFoundException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void dropMiddleAlternativeShadowedByEarlierCatch() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException | FileNotFoundException | IllegalStateException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException | IllegalStateException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void dropSubtypeAlternativeShadowedByEarlierCatch() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      } catch (FileNotFoundException | RuntimeException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      } catch (RuntimeException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void dropEntireCatchWhenAllAlternativesAreShadowed() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new IOException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      } catch (FileNotFoundException | java.io.EOFException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new IOException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void dropEntireSingleTypeCatchWhenShadowed() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new IOException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """,
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new IOException();
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNothingWhenOrderingMeansNothingIsShadowed() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+              import java.io.FileNotFoundException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new FileNotFoundException();
+                      } catch (FileNotFoundException e) {
+                          System.out.println(e);
+                      } catch (IOException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNothingWhenMultiCatchAlternativesAreAllNeeded() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import java.io.IOException;
+
+              class A {
+                  void m() {
+                      try {
+                          throw new IOException();
+                      } catch (IOException | IllegalStateException e) {
+                          System.out.println(e);
+                      }
+                  }
+              }
+              """
+          )
+        );
+    }
+}
