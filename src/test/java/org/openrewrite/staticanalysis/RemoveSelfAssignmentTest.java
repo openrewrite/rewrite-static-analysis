f9286ff52aca2a017c0639c0faa31e43cd9fe049
Greg Oledzki <greg.oledzki@moderne.io>
2026-07-13 15:39:44 +0200

    Add Go tests for selected 7 recipes

--- src/test/java/org/openrewrite/staticanalysis/RemoveSelfAssignmentTest.java
+++ src/test/java/org/openrewrite/staticanalysis/RemoveSelfAssignmentTest.java
@@ -23,6 +23,7 @@ import org.openrewrite.java.tree.J;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
+import static org.openrewrite.golang.Assertions.go;
 import static org.openrewrite.groovy.Assertions.groovy;
 import static org.openrewrite.java.Assertions.java;
 import static org.openrewrite.javascript.Assertions.typescript;
@@ -276,6 +277,42 @@ class RemoveSelfAssignmentTest implements RewriteTest {
             """
               x = 1
               print(x)
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeSelfAssignmentGo() {
+        rewriteRun(
+          //language=go
+          go(
+            """
+              package main
+
+              type Profile struct {
+                  Name string
+              }
+
+              func normalize(profile *Profile) {
+                  profile.Name = profile.Name
+                  trimName(profile)
+              }
+
+              func trimName(profile *Profile) {}
+              """,
+            """
+              package main
+
+              type Profile struct {
+                  Name string
+              }
+
+              func normalize(profile *Profile) {
+                  trimName(profile)
+              }
+
+              func trimName(profile *Profile) {}
               """
           )
         );
